14d06323aae857e40214a952e5fa17e0a2d36c06
wayan.saputra <wayan.saputra@bps.go.id>
2020-01-07 14:56:53 +0800

    add active class

--- src/components/Sidemenu.jsx
+++ src/components/Sidemenu.jsx
@@ -3,7 +3,7 @@ import React from 'react'
 export default function Sidemenu() {
     return (
         <div style={{width: "48px", height: "100%", backgroundColor: "#333333", display: "flex", flexDirection: "column"}}>
-            <a className="sideButton">&#xEAF0;</a>
+            <a className="sideButton active">&#xEAF0;</a>
             <a className="sideButton">&#xEA6D;</a>
             <a className="sideButton">&#xEA68;</a>
             <a className="sideButton">&#xEAD8;</a>
